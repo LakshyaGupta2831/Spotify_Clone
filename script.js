41968721f2c8f9f2ef91ff22a8ca81461cfd9a1d
Lakshya Gupta <lakshyagupta2903@gmail.com>
2025-02-18 23:52:29 +0530

    New script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log('Welcome to Spotify');
 
 // Initialize the Variables
 let songIndex = 0;
-let audioElement = new Audio('1.mp3');
+let audioElement = new Audio('video-img1/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
@@ -11,16 +11,16 @@ let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
-    {songName: "Ainsi bas la vida - Mini World (Deluxe) by Indila", filePath: '1.mp3', coverPath: 'song-1.jpg'},
-    {songName: "Faling - Trevor Daniel ", filePath: '2.mp3', coverPath: '2.jpg'},
-    {songName: "Stereo Hearts (feat. Adam Levine) -  Gym Class Heroes", filePath: '3.mp3', coverPath: '3.jpg'},
-    {songName: "Into Your Arms -  Witt Lowry (ft. Ava Max)", filePath: '4.mp3', coverPath: '4.jpg'},
-    {songName: "Alone, Pt. II - (ft. Alan Walker, Ava Max )", filePath: '5.mp3', coverPath: '5.jpg'},
-    {songName: "Danza Kuduro - Don Omar", filePath: '6.mp3', coverPath: '6.jpg'},
-    {songName: "Let Me Down Slowly - Alec Benjamin ", filePath: '7.mp3', coverPath: '7.jpg'},
-    {songName: "London View Bootleg - Emin Nilsen", filePath: '8.mp3', coverPath: '8.jpg'},
-    {songName: "Wellerman (Sea Shanty) -  Nathan Evans", filePath: '9.mp3', coverPath: '9.jpg'},
-    {songName: "Hymn for the Weekend - Coldplay", filePath: '10.mp3', coverPath: '10.jpg'}
+    {songName: "Ainsi bas la vida - Mini World (Deluxe) by Indila", filePath: 'video-img1/1.mp3', coverPath: 'video-img1/song-1.jpg'},
+    {songName: "Faling - Trevor Daniel ", filePath: 'video-img1/2.mp3', coverPath: 'video-img1/2.jpg'},
+    {songName: "Stereo Hearts (feat. Adam Levine) -  Gym Class Heroes", filePath: 'video-img1/3.mp3', coverPath: 'video-img1/3.jpg'},
+    {songName: "Into Your Arms -  Witt Lowry (ft. Ava Max)", filePath: 'video-img1/4.mp3', coverPath: 'video-img1/4.jpg'},
+    {songName: "Alone, Pt. II - (ft. Alan Walker, Ava Max )", filePath: 'video-img1/5.mp3', coverPath: 'video-img1/5.jpg'},
+    {songName: "Danza Kuduro - Don Omar", filePath: 'video-img2/6.mp3', coverPath: 'video-img2/6.jpg'},
+    {songName: "Let Me Down Slowly - Alec Benjamin ", filePath: 'video-img2/7.mp3', coverPath: 'video-img2/7.jpg'},
+    {songName: "London View Bootleg - Emin Nilsen", filePath: 'video-img2/8.mp3', coverPath: 'video-img2/8.jpg'},
+    {songName: "Wellerman (Sea Shanty) -  Nathan Evans", filePath: 'video-img2/9.mp3', coverPath: 'video-img2/9.jpg'},
+    {songName: "Hymn for the Weekend - Coldplay", filePath: 'video-img2/10.mp3', coverPath: 'video-img2/10.jpg'}
 ];
 
 songItems.forEach((element, i)=>{
